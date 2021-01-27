2791320e6366f00fe6ba947e9901763c5806b19f
Jito Chadha <jitochadha@gmail.com>
2021-01-27 11:35:37 +0200

    finalized app

--- Develop/script.js
+++ Develop/script.js
@@ -1,46 +1,83 @@
-$(document).ready(function(){
+$(document).ready(function () {
+     
+  
     function updateTime() {
-        let currentTime = $("#currentDay");
-        let now = new Date();
-        currentTime.text(now.toLocaleTimeString());
-        let currentHour = now.getHours();
-        console.log(currentHour);
-        
-    }; 
+      let currentTime = $('#currentDay');
+      currentTime.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+      
+      
+    }
+  
     function formatCalendar() {
-        for (let i = 9; i <= 17; i++) {
-            formatTimeBlock(i);
-        }
-        
-    };
+      for (let i = 9; i <= 17; i++) {
+        formatTimeBlock(i);
+      }
+    }
+  
     function formatTimeBlock(hour) {
-        let timeBlock = $(`#time-block-${hour}`);
-        let currentHour = new Date().getHours();
-        currentHour = 13;
-        if (hour < currentHour) {
-            timeBlock.addClass("past");
-        } else if (hour < currentHour + 1) {
-            timeBlock.addClass("present");
+      let timeBlock = $(`#time-block-${hour}`);
+      let currentHour = new Date().getHours();
+      if (hour < currentHour) {
+        timeBlock.addClass('past');
+      } else if (hour < currentHour + 1) {
+        timeBlock.addClass('present');
+      } else {
+        timeBlock.addClass('future');
+      }
+    }
+  
+    function handleSaveEvent(hour) {
+      let newTaskDetails = $(`#time-block-${hour} textarea`).val();
+      tasks = tasks.map(function (task) {
+        if (task.hour === hour) {
+          let newTask = task;
+          newTask.taskDetails = newTaskDetails;
+          return newTask;
         } else {
-            timeBlock.addClass("future");
+          return task;
         }
-
-        timeBlock.addClass("past")
-
-    };
-
-    function handleSaveEvent(event) {
-        console.log(event);
-    };
-
+      });
+      saveTasks();
+      displayTasks();
+    }
+  
     function initializeSaveButtons() {
-        // for (let i = )
-        $("#time-block-9 button").click(handleSaveEvent);
-        
-    };
-
+      for (let i = 9; i <= 17; i++) {
+        $(`#time-block-${i} button`).click(function () {
+          handleSaveEvent(i);
+        });
+      }
+    }
+  
+    function loadTasks() {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+      if (!tasks || tasks.length === 0) {
+        tasks = [];
+        for (let i = 9; i <= 17; i++) {
+          tasks.push({ hour: i, taskDetails: '' });
+        }
+      }
+    }
+  
+    function displayTasks() {
+      for(let task of tasks) {
+        $(`#time-block-${task.hour} textarea`).text(`${task.taskDetails}`)
+      }
+    }
+  
+    function initializeTasks() {
+      loadTasks();
+      displayTasks();
+    }
+  
+    function saveTasks() {
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+  
     formatCalendar();
     updateTime();
     setInterval(updateTime, 1000);
     initializeSaveButtons();
-});
+    initializeTasks();
+  });
+  
