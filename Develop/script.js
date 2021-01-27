07dc2e0fb185ce78cb814f92fbb5bfc06981559e
Jito Chadha <jitochadha@gmail.com>
2021-01-27 00:11:54 -0800

    added some funcition for time and event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -6,13 +6,41 @@ $(document).ready(function(){
         let currentHour = now.getHours();
         console.log(currentHour);
         
-    } 
-    function formatTimeBlock() {
-        let timeBlock = $("#time-block-9");
+    }; 
+    function formatCalendar() {
+        for (let i = 9; i <= 17; i++) {
+            formatTimeBlock(i);
+        }
+        
+    };
+    function formatTimeBlock(hour) {
+        let timeBlock = $(`#time-block-${hour}`);
+        let currentHour = new Date().getHours();
+        currentHour = 13;
+        if (hour < currentHour) {
+            timeBlock.addClass("past");
+        } else if (hour < currentHour + 1) {
+            timeBlock.addClass("present");
+        } else {
+            timeBlock.addClass("future");
+        }
+
         timeBlock.addClass("past")
-    }
 
+    };
 
+    function handleSaveEvent(event) {
+        console.log(event);
+    };
+
+    function initializeSaveButtons() {
+        // for (let i = )
+        $("#time-block-9 button").click(handleSaveEvent);
+        
+    };
+
+    formatCalendar();
     updateTime();
     setInterval(updateTime, 1000);
+    initializeSaveButtons();
 });
